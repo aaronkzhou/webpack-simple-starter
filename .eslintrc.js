43172393f00b25031dd8e144114f0d497f655b64
Aaron Zhou <aaaron.chou@gmail.com>
2018-05-10 20:26:15 +1200

    Move config file to package.json

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,7 @@ module.exports = {
     browser: true,
   },
   // https://github.com/feross/standard/blob/master/RULES.md#javascript-standard-style
-  extends: 'airbnb',
+  extends: '',
   // required to lint *.vue files
   plugins: [
     'html'
